10be66a3bb513908c9a47debd7288f1d5cdb2514
centralfielder <lucabisin@gmail.com>
2017-07-18 14:09:27 +0100

    minor

--- src/main/java/lbsn/twitter_orm_app/sentiment/SentimentClassifier.java
+++ src/main/java/lbsn/twitter_orm_app/sentiment/SentimentClassifier.java
@@ -1,6 +1,5 @@
 package lbsn.twitter_orm_app.sentiment;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import uk.ac.wlv.sentistrength.SentiStrength;
